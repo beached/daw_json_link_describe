4ac54fb016f16134b175fa28071bb0e093bcb598
Darrell Wright <beached@users.noreply.github.com>
2022-05-06 02:52:57 -0400

    Added map examples

--- tests/src/daw_json_link_describe_test.cpp
+++ tests/src/daw_json_link_describe_test.cpp
@@ -11,6 +11,8 @@
 #include <boost/describe.hpp>
 #include <cassert>
 #include <iostream>
+#include <map>
+#include <optional>
 
 struct X {
 	int m1;
@@ -23,6 +25,17 @@ struct Y {
 	std::string m1;
 };
 BOOST_DESCRIBE_STRUCT( Y, ( ), ( m0, m1 ) );
+
+struct Z {
+	std::map<std::string, int> kv;
+};
+BOOST_DESCRIBE_STRUCT( Z, ( ), ( kv ) );
+
+struct Foo {
+	std::optional<Y> m0;
+	std::vector<X> m1;
+};
+BOOST_DESCRIBE_STRUCT( Foo, ( ), ( m0, m1 ) );
 
 int main( ) {
 	constexpr daw::string_view json_doc0 = R"json(
@@ -48,4 +61,37 @@ int main( ) {
 	assert( val1.m0.m2 == 123 );
 	assert( val1.m1 == "Hello World!" );
 	std::cout << "json: " << daw::json::to_json( val1 ) << '\n';
+
+	constexpr daw::string_view json_doc2 = R"json(
+{
+	"kv": { "key0": 0, "key1": 1, "key2": 2 },
+}
+)json";
+	auto val2 = daw::json::from_json<Z>( json_doc2 );
+	auto p = val2.kv.find( "key0" );
+	assert( p != val2.kv.end( ) );
+	assert( p->second == 0 );
+
+	p = val2.kv.find( "key1" );
+	assert( p != val2.kv.end( ) );
+	assert( p->second == 1 );
+
+	p = val2.kv.find( "key2" );
+	assert( p != val2.kv.end( ) );
+	assert( p->second == 2 );
+	std::cout << "json: " << daw::json::to_json( val2 ) << '\n';
+
+	constexpr daw::string_view json_doc3 = R"json(
+{
+	"m1": [ { "m1": 0, "m2": 1 }, { "m1": 2, "m2": 3 } ]
+}
+)json";
+	auto val3 = daw::json::from_json<Foo>( json_doc3 );
+	assert( not val3.m0 );
+	assert( val3.m1.size( ) == 2 );
+	assert( val3.m1[0].m1 == 0 );
+	assert( val3.m1[0].m2 == 1 );
+	assert( val3.m1[1].m1 == 2 );
+	assert( val3.m1[1].m2 == 3 );
+	std::cout << "json: " << daw::json::to_json( val3 ) << '\n';
 }
